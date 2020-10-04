b3bf6b36f0870d58cde7a72e5eacc83e92fe53e3
Cameus <hans@MacBook-Pro.local>
2020-10-04 21:03:09 +0200

    hans

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react';
 import './App.css';
 import Home from "./component/Home"
-import {Switch,Route, Redirect,Link} from "react-router-dom"
+import {Switch,Route, Redirect} from "react-router-dom"
 import Shop from "./component/shop"
 import Smile from "./component/smile"
 import Choice from "./component/choice"
@@ -15,44 +15,14 @@ import count from "./reducers/count.reducer"
 
 
 const store = createStore(combineReducers({count}),applyMiddleware(logger));
-
-
-
+  
 function App() {
   const [infos,setInfo] = useState("");
-
 
   const useStyles = makeStyles({
     logo:{
       width:"5%",
-      height:"3,5rem",
-      marginLeft:"8%",
-      marginRight:"20%"
-      
-    },
-    bouton:{
-      textDecoration:"none",
-      fontFamily:"Sansita Swashed, cursive",
-      display:"flex",
-      justifyContent:"space-between",
-      color:"black",
-    
-
-    },
-    homeI:{
-      marginLeft:"5%",
-      width:"5%",
-      height:"40%"
-    },
-
-    lien:{
-      textDecoration:"none"
-    },
-
-    menu:{
-      display:"flex",
-      justifyContent:"space-around",
-      width:"50%"
+      height:"60%"
     }
   });
 
